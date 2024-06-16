d2e2081571f7093dcdb3a96f8081ae0f5313582c
zoeznm <bomikim046@gmail.com>
2024-06-16 15:49:05 +0900

    끝

--- js/tab.js
+++ js/tab.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-
+  $(function(){
+    $('.tabMenu>li>a').click(function(){
+        $(this).parent().addClass("active").siblings().removeClass("active");
+       return false;
+      });
+    });
 });
